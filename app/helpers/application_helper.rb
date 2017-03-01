8f06d5b60bcab560630e947425a26c40d544146f
Aleksey Protsyk <alexprotsyk@gmail.com>
2017-03-01 03:41:38 +0300

    Changed list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Возвращает полный заголовок зависящий от страницы # Документирующий комментарий
   def full_title(page_title)                          # Определение метода
-    base_title = "Ruby on Rails Tutorial Sample App"  # Назначение переменной
+    base_title = "Префиксы и Суффиксы PoE"  # Назначение переменной
     if page_title.empty?                              # Булевый тест
       base_title                                      # Явное возвращение
     else
