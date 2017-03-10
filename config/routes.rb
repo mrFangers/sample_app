f71a1808df8c44e17fa5ff2019eac2b7499a7a6f
Aleksey Protsyk <alexprotsyk@gmail.com>
2017-03-10 11:49:54 +0300

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/odnoruchnyj_topor', to: 'static_pages#one_hand_axe', via: 'get'
+  match '/odnoruchnyj-topor', to: 'static_pages#one_hand_axe', via: 'get'
+  match '/kogti', to: 'static_pages#kogti', via: 'get'
+  match '/odnoruchnyj-kinzhal', to: 'static_pages#kinzhal', via: 'get'
+  match '/odnoruchnaya-bulava', to: 'static_pages#onehandmace', via: 'get'
+  match '/skipetr', to: 'static_pages#skipetr', via: 'get'
+  match '/odnoruchnyj-mech', to: 'static_pages#one_hand_sword', via: 'get'
+  match '/zhezl', to: 'static_pages#zhezl', via: 'get'
 end
